4002e4bfa953aa4adfb2bcdb00246b3c2ca93db8
Denis Sergeevich <denissuspicin@gmail.com>
2020-09-06 15:06:11 +0300

    initial commit

---
+++ HapticFeedbackTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HapticFeedbackTests
+//
+//  Created by DenisSuspitsyn on 06.09.2020.
+//  Copyright © 2020 Denis Suspicin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
